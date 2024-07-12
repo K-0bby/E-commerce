9a7eb06fcbeab175e0094256f44db76d6c93ddba
K-0bby <k.hoenyefia@gmail.com>
2024-07-12 15:25:16 +0000

    update

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+
+
+
+const ContactForm = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Submitted:', formData )
+    }
+  return (
+    <div className='max-w-md'>
+        <form onSubmit={handleSubmit}>
+        <div className="mb-4 font-montserrat">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="border w-full py-2 px-3 text-gray-700 leading-tight outline-none"
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div className="mb-4 font-montserrat">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className=" border w-full py-2 px-3 text-gray-700 leading-tight outline-none "
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="mb-4 font-montserrat">
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="border w-full h-[200px] py-2 px-3 text-gray-700 leading-tight outline-none"
+            placeholder="Message"
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="border border-slate-950 hover:bg-orange-400 hover:border-orange-400 hover:text-white text-black uppercase text-xs tracking-[2px] py-3 px-4 font-montserrat"
+          >
+            Send 
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import ContactForm from '../components/ContactForm';
 
 const Contact = () => {
   return (
     <div>
     <Navbar />
-        Contact
+      <div className='h-[450px] relative overflow-hidden w-full'
+        style={{
+          backgroundImage: 'url(/images/blog/1.webp)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          objectFit: 'cover' 
+        }}
+      >
+        <div className='w-full h-full'>
+          <div className="absolute inset-0 bg-black bg-opacity-45 flex flex-col items-center justify-center text-center p-4">
+            <h1 className="text-white text-7xl font-bold mt-28 mb-4 font-cormorant italic">Contact Us</h1>
+            {/* <p className="text-gray-200 text-sm w-2/3 mb-4 font-montserrat">Discover our latest collection</p> */}
+          </div>
+        </div>
+      </div>
+      
+      <div className='container mx-auto py-16 px-8 xl:px-28 text-center flex flex-col justify-center items-center mt-10'>
+        <h6 className='uppercase font-montserrat text-xs font-semibold tracking-[2px] mb-5'>Get in Touch</h6>
+        <h4 className='font-cormorant italic capitalize font-bold text-3xl  w-1/2 mb-1'>We Value The Connection With Our Community & Customers And Are Here To Assist In Any Way We Can. Feel Free To Reach Out Through The Following Channels:</h4>
+      </div>
+      <div className='py-16 px-8 xl:px-28 text-center flex flex-col justify-between '>
+        <ContactForm />
+        <div></div>
+      </div>
     </div>
   )
 }
