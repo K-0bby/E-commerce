204b1acbecbecbda1c8394077222a3930b559f69
K-0bby <k.hoenyefia@gmail.com>
2024-07-17 20:42:12 +0000

    update

--- src/components/BackGroundSlider.jsx
+++ src/components/BackGroundSlider.jsx
@@ -36,7 +36,7 @@ const BackGroundSlider = () => {
             >
               <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center p-4">
                 <h6 className="text-white text-sm uppercase mb-2 font-montserrat">{slide.title}</h6>
-                <h1 className="text-white text-7xl font-bold mb-4 font-cormorant italic" dangerouslySetInnerHTML={{ __html: slide.subtitle }}></h1>
+                <h1 className="text-white text-5xl font-bold mb-4 font-cormorant italic" dangerouslySetInnerHTML={{ __html: slide.subtitle }}></h1>
                 <p className="text-gray-200 text-sm w-2/3 mb-4 font-montserrat">{slide.description}</p>
                 <Link to={slide.link} className='bg-transparent border border-[#ff7f50] text-white px-7 py-3 mt-4 uppercase rounded-sm text-sm'>
                   <span className="text-[#ff7f50] font-montserrat">{slide.linkText}</span>
